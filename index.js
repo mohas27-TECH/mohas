c725deb378795a8e2091f1c05e20ab3efee3c93b
mohas27-TECH <fayomimuhammad@gmail.com>
2024-10-21 09:57:12 -0700

    updated with responsiveness

--- index.js
+++
@@ -1,7 +0,0 @@
-let menu = document.querySelector('#menu-icon');
-let navist= document.querySelector('.navlist');
-
-menu.onclick = () => {
-    menu.classlist.toggle('bx-x');
-    navist.classList.toggle('open');
-}
